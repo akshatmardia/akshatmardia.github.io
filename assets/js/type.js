0c102570f8382a2b4e50e88599c47c06b859dc10
Akshat Mardia <Akshat@MacBook-Pro-329.local>
2024-08-08 20:14:51 -0400

    footer

---
+++ assets/js/type.js
@@ -0,0 +1,22 @@
+var i = 0;
+var content = "I'm Akshat Mardia. I am an engineer from the UW in Seattle. Mount Rainier bitches!";
+var speed = 80;
+
+function animation() {
+
+    if (i < content.length) {
+        document.getElementById("typewriter").innerHTML += content.charAt(i);
+        i++;
+        setTimeout(animation, speed);
+    } else {
+        document.getElementById("typewriter").innerHTML += '<span class="blinking-cursor">|</span>';
+    }
+}
+
+// window.onload = function() {
+//     animation();
+// };
+
+document.addEventListener("DOMContentLoaded", function() {
+    animation();
+});
