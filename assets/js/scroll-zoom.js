85c63ddefc6706d6439aedbf33188780d08869ae
Akshat Mardia <Akshat@MacBook-Pro-329.local>
2024-08-13 23:22:31 -0700

    new design

---
+++ assets/js/scroll-zoom.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const { innerHeight } = window;
+
+    gsap.from(".title-box", {
+        scale: 50, 
+        y: 0,  // Start centered
+        stagger: 0.25, 
+        duration: 3,
+        scrollTrigger: {
+            trigger: ".hero-image",
+            pin: true,
+            start: "top top",  // Start when the top of the section reaches the top of the viewport
+            end: `+=${innerHeight * 2}`,  // Extend the end point to give enough space for the animation
+            scrub: 0
+        }
+    });
+
+    gsap.to(".hero-image", {
+        scrollTrigger: {
+            trigger: ".main-content",  // When the next section starts
+            start: "top top",  // Start pinning as the next section's top reaches the top
+            pin: true,
+            pinSpacing: false, // No additional space added when pinning
+            scrub: 0
+        }
+    });
+});
+
--- assets/js/type.js
+++ assets/js/type.js
@@ -1,9 +1,8 @@
 var i = 0;
-var content = "I'm Akshat Mardia. I am an engineer from the UW in Seattle. Mount Rainier bitches!";
+var content = "I am an engineer from the UW in Seattle. Mount Rainier bitches!";
 var speed = 80;
 
 function animation() {
-
     if (i < content.length) {
         document.getElementById("typewriter").innerHTML += content.charAt(i);
         i++;
